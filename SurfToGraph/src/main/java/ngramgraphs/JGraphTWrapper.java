e4baf080880c2fa4fe40cc5138fbf35d61404326
sanantoniochili <to-tsi@windowslive.com>
2018-09-26 15:28:43 +0300

    OpenJGraph to JGraphT

---
+++ SurfToGraph/src/main/java/ngramgraphs/JGraphTWrapper.java
@@ -0,0 +1,36 @@
+package ngramgraphs;
+
+import org.jgrapht.Graph;
+import org.jgrapht.alg.connectivity.ConnectivityInspector;
+import org.jgrapht.alg.shortestpath.BidirectionalDijkstraShortestPath;
+import org.jgrapht.alg.shortestpath.FloydWarshallShortestPaths;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DirectedMultigraph;
+import org.jgrapht.graph.DirectedPseudograph;
+import org.jgrapht.io.*;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+
+import java.io.StringReader;
+import java.util.Set;
+
+public class JGraphTWrapper {
+
+    Graph G;
+
+    public Graph convertString(String graph) throws ImportException {
+        // convert DOT string to JGraphT object
+        VertexProvider<String> vp = (label, attrs) -> label;
+        EdgeProvider<String, DefaultEdge> ep = (f, t, l, attrs) -> new DefaultEdge();
+        ComponentUpdater<String> cu = (v, attrs) -> {
+        };
+        DOTImporter<String, DefaultEdge> importer = new DOTImporter<>(vp, ep, cu);
+
+        DirectedPseudograph<String, DefaultEdge> jgraph =
+                new DirectedPseudograph<>(DefaultEdge.class);
+        importer.importGraph(jgraph, new StringReader(graph));
+
+        this.G = jgraph;
+
+        return jgraph;
+    }
+}
--- SurfToGraph/src/main/java/ngramgraphs/Main.java
+++ SurfToGraph/src/main/java/ngramgraphs/Main.java
@@ -3,12 +3,7 @@ package ngramgraphs;
 import gr.demokritos.iit.jinsect.documentModel.representations.DocumentNGramGraph;
 import gr.demokritos.iit.jinsect.utils;
 import input_load.TextReader;
-
-import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.DirectedMultigraph;
-import org.jgrapht.graph.DirectedPseudograph;
-import org.jgrapht.io.*;
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.io.ImportException;
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -30,22 +25,11 @@ public class Main {
              /* The following command gets the first n-gram graph level (with the minimum n-gram
             size) and renders it, using the utils package, as a DOT string */
             String graph =  utils.graphToDot(dngGraph.getGraphLevel(0), true);
-    // System.out.println(graph);
 
-            // convert DOT string to JGraphT object
-            VertexProvider<String> vp = (label, attrs) -> label;
-            EdgeProvider<String, DefaultEdge> ep = (f, t, l, attrs) -> new DefaultEdge();
-            ComponentUpdater<String> cu = (v, attrs) -> {
-            };
-            DOTImporter<String, DefaultEdge> importer = new DOTImporter<>(vp, ep, cu);
+            JGraphTWrapper jgraph = new JGraphTWrapper();
+            jgraph.convertString(graph);
+            jgraph.ops();
 
-            DirectedPseudograph<String, DefaultEdge> jgraph =
-                    new DirectedPseudograph<>(DefaultEdge.class);
-            importer.importGraph(jgraph, new StringReader(graph));
-
-    // System.out.println(jgraph.toString());
-
-            DijkstraShortestPath dpath = new DijkstraShortestPath(jgraph);
 
         //}while( surf!=null ); // for all surface-texts
     }
